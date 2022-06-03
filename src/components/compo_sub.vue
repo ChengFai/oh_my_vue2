996015b273bae5aa6794678e2faa26cb388955e3
2519132990@qq.com <ChengFai@users.noreply.github.com>
2022-06-03 10:25:25 +0800

    开始使用echarts

--- src/components/compo_sub.vue
+++ src/components/compo_sub.vue
@@ -1,30 +1,34 @@
 <template>
     <div>
-        <h3>当前币安的聚合行情涨幅榜：</h3>
+        <h3>交易所行情</h3>
         <table>
+          币安
           <tbody>
             <tr>
               <th>排名</th>
               <th>币种</th>
               <th>涨幅</th>
             </tr>
-            <tr v-for="(item, index) in topList" :key="item[0]">
+            <tr v-for="(item, index) in binanceTopList" :key="item[0]">
               <td>{{index + 1}}</td>
               <td>{{item[0]}}</td>
               <td>{{item[1]}}</td>
             </tr>
           </tbody>
         </table>
+        <div id="myChart" style="width: 600px;height:400px;"></div>
     </div>
 </template>
 
 <script>
+let timer, last_time ;
 
 export default {
   data () {
     return {
       btc_price: "0.00",
-      topList: [],
+      binanceTopList: [],
+      okTopList: [],
       dataList: [],
       usdt_list: [],
       symbol_list: [],
@@ -33,7 +37,29 @@ export default {
     }
   },
   created () {
-    this.symbol_list = localStorage.getItem("symbolList").split(",");
+    let myChart = this.echarts.init(document.getElementById('myChart'));
+    // 绘制图表
+    myChart.setOption({
+        title: {
+            text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        xAxis: {
+            data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [{
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+        }]
+    });
+
+    this.symbol_list = localStorage.getItem("symbolList").split(",").filter(
+      (item) => {
+        return !(item.includes("UPUSDT") || item.includes("DOWNUSDT"))
+      }
+    )
     this.symbol_list.forEach(item => {
       this.change_map.set(item, "0.00");
     });
@@ -62,6 +88,21 @@ export default {
       // 数据接收
       websocketonmessage(e){
         let data = JSON.parse(e.data);
+        let now = new Date().getTime();
+        if(last_time && now - last_time < 2000){
+          return;
+        }
+        if(!timer) 
+        {
+          last_time = now;
+          timer = setTimeout(() => {
+            this.binanceDataHd(data);
+            timer = null;
+          }, 2000);
+        }
+
+      },
+      binanceDataHd(data){
         if(Array.isArray(data)){
           data.forEach(item => {
             if(this.change_map.has(item.s)){
@@ -69,16 +110,15 @@ export default {
               }
           });
         }
-        this.topList = Array.from(this.change_map).sort((a, b) => {
+        this.binanceTopList = Array.from(this.change_map).sort((a, b) => {
           return  b[1] - a[1];
         }).filter((item) => {
-          return item[0].substr(-8) !== "DOWNUSDT";
+          return (item[0].substr(-8) !== "DOWNUSDT" && item[0].substr(-6) !== "UPUSDT");
         }).slice(0, 20).map((item) => {
-          return [item[0], item[1] + "%"];
+          return [item[0].replace("USDT",""), item[1] + "%"];
         });
-        console.log(this.topList)
       },
-      setTopList(latestPrice){
+      setbinanceTopList(latestPrice){
         if(latestPrice)
         {
           this.btc_price = latestPrice.substring(0,latestPrice.indexOf(".") + 3);
@@ -100,5 +140,19 @@ export default {
 
 </script>
 
+
+<style scoped>
+table{
+  width: 400px;
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+
+td, th {
+  border: 1px solid rgb(128, 128, 128);
+  text-align: center;
+  width: auto;
+}
+</style>
 
       
--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+import * as echarts from "echarts";
 
 Vue.config.productionTip = false
 Vue.prototype.axios = axios
+Vue.prototype.echarts = echarts;
 
 new Vue({
   router,
